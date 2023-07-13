ce1dc48a09a87d0329a7c93903b3ebcc6b2f6157
cocoO <glenardrefamonte1@gmail.com>
2023-07-13 23:22:59 +0800

    first commit

---
+++ beshy-app/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import ColorModeComponent from "./components/ColorModeComponent";
+import { Flex, HStack, Input, Stack, Text, useToast } from "@chakra-ui/react";
+import CardComponent from "./components/CardComponent";
+import "./App.css";
+function App() {
+  const [text, setText] = useState("");
+  const [generatedText, setGeneratedText] = useState("");
+  const toast = useToast();
+
+  const generateText = () => {
+    const trimmedText = text.trim();
+
+    if (trimmedText.length > 0) {
+      const replacedValue = trimmedText.replace(/\s+/g, " ");
+      const newValue = replacedValue.replace(/\s/g, "🤸");
+      setText(newValue);
+      return newValue;
+    } else {
+      setText(trimmedText);
+    }
+  };
+
+  const copyTextUsingTextArea = (text) => {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  };
+
+  const copyText = () => {
+    copyTextUsingTextArea(text);
+    toast({
+      title: "🤸 Beshify 🤸",
+      description: "Text Copied Successfully.",
+      status: "success",
+      duration: 500,
+      isClosable: true,
+    });
+  };
+
+  return (
+    <>
+      <Flex
+        height="100vh"
+        width="100%"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack>
+          <Text
+            fontSize={{ base: "4xl", md: "4xl", lg: "6xl" }}
+            fontFamily={"Shrikhand, cursive"}
+          >
+            🤸 Beshify 🤸
+          </Text>
+          <Stack>
+            <HStack>
+              <Input
+                placeholder="Enter text..."
+                onChange={(e) => setText(e.target.value)}
+              />
+              <ColorModeComponent />
+            </HStack>
+
+            {text.length > 0 && (
+              <CardComponent
+                text={text}
+                generateText={generateText}
+                copyText={copyText}
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Flex>
+    </>
+  );
+}
+
+export default App;
